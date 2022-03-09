fe3ef00a34fe4f461ed62c0bcfcb001d81fee847
icubo0216 <yuki_ichigo0216@icloud.com>
2022-03-09 19:22:09 +0900

    タイムの大まかな設定 タイトル移行後の設定画面の雛形作成

---
+++ setting.h
@@ -0,0 +1,36 @@
+//------------------------------
+//設定のヘッダー
+//ACG000
+//Author: goto yuuki
+//
+//------------------------------
+#ifndef _SETTING_H_		//このマクロが定義されてなかったら
+#define _SETTING_H_		//2重インクルード防止のマクロ定義
+
+#include "main.h"
+#include "time.h"
+#include "math.h"
+
+//プロトタイプ宣言
+void InitSetting(void)
+{
+
+}
+void UninitSetting(void)
+{
+
+}
+void UpdateSetting(void)
+{
+
+}
+void DrawSetting(void)
+{
+
+}
+void SetSetting(void)
+{
+
+}
+
+#endif
