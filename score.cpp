2e16ab846d633fbb5cdadf2c0799ef2665f4ba11
icubo0216 <yuki_ichigo0216@icloud.com>
2022-03-23 08:50:08 +0900

    更新

--- score.cpp
+++ score.cpp
@@ -70,10 +70,10 @@ void InitScore(void)
 			pVtx[3].rhw = 1.0f;
 
 			//頂点カラーの設定
-			pVtx[0].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
-			pVtx[1].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
-			pVtx[2].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
-			pVtx[3].col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+			pVtx[0].col = D3DXCOLOR(1.0f, 1.0f, 0.0f, 1.0f);
+			pVtx[1].col = D3DXCOLOR(1.0f, 1.0f, 0.0f, 1.0f);
+			pVtx[2].col = D3DXCOLOR(1.0f, 1.0f, 0.0f, 1.0f);
+			pVtx[3].col = D3DXCOLOR(1.0f, 1.0f, 0.0f, 1.0f);
 
 			//テクスチャの座標設定
 			pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
